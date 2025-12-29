63dbd635cdd426ae768d9f2b78d5276f8023b4ce
nanihaeranie <nanihaerani255@gmail.com>
2025-12-29 11:29:15 +0700

    perbaikan title seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ const IndexPage = () => (
   <div className="h-auto w-screen">
     <Layout>
       <Seo
-        title="Holo - Gatsby Theme"
-        description="Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS."
+        
+        description="Al-Miftahul Huda."
       ></Seo>
       <Header></Header>
       <FeatureSection></FeatureSection>
